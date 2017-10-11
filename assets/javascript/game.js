4c3fc6dd87065cec77f4a58ee6c01b1ad8fbabb2
boronflex <kyle_palmer@icloud.com>
2017-10-10 23:32:53 -0500

    game works as-is, could be cleaned up, char stats need to be edited and losing needs to be tested

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -49,15 +49,8 @@
 //   }
 // }
 
-//need to get the characters to fight
-  //rules:
-  // * Each time the player attacks, their character's Attack Power increases by its base Attack Power. 
-  //   * For example, if the base Attack Power is 6, each attack will increase the 
-  //Attack Power by 6 (12, 18, 24, 30 and so on).
-    //get and store hp, ap, cap from main character
 
-
-
+//can possibly clean this up with a while loop
   var mainAttackPointsIncrease = 0;
 
   $("#attack-button").click(function () {
@@ -76,61 +69,38 @@
     getMainAttackPoints = getMainAttackPoints * mainAttackPointsIncrease;
 
     var damageYou = getMainHeathPoints - getDefendCap;
-    //edit your hp
     mainCharacter.attr("hp", damageYou);
-    $("#your-attack-text").text("You attacked them for " + getMainAttackPoints + " damage");
     mainCharacter.find(".character-hp").text(damageYou);
+    var yourCharacter = mainCharacter.find(".character-name").text();
 
     var damageThem = getDefendHealthPoints - getMainAttackPoints;
-    //edit their hp
     defendCharacter.attr("hp", damageThem);
-    $("#enemy-attack-text").text("They attacked you back for " + getDefendCap + " damage")
     defendCharacter.find(".character-hp").text(damageThem);
+    var defendCharacter = defendCharacter.find(".character-name").text();
     
-    //TODO:
 
-    //if loop for if you go to zero or below
-    //else if for if they go to zero or below
-      //next need to use .remove() get rid of defeated enemies
+    $("#your-attack-text").text(yourCharacter + " attacked " + defendCharacter + " for " + getMainAttackPoints + " damage");
+    $("#enemy-attack-text").text(defendCharacter + " attacked " + yourCharacter + " for " + getDefendCap + " damage");
+
+    //might need to move this out and move variables global, works but not pretty
+    if (damageYou <= 0){
+      $("#your-attack-text").text("You have been defeated!!");
+      $("#restartButton").css({"visibility":"visible"});
+
+    }
+
+    if (damageThem <= 0){
+
+      getDefendCap = 0;
+
+      $("div[state = 'defender']").remove();
+      $("#your-attack-text").text("You have defeated them, you can chose another enemy.");
+      $("#enemy-attack-text").css({"visibility":"hidden"});
+    }
 
   });
 
+  $( "#restartButton" ).click(function() {
+    location.reload(true);
+  });
 
-// 2. Here's how the app works:
-
-//    * When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game.
-
-//    * The player must then defeat all of the remaining fighters. Enemies should be moved to a different area of the screen.
-
-//    * The player chooses an opponent by clicking on an enemy's picture.
-
-//    * Once the player selects an opponent, that enemy is moved to a `defender area`.
-
-//    * The player will now be able to click the `attack` button.
-//      * Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture. 
-//      * The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture.
-
-// 3. The player will keep hitting the attack button in an effort to defeat their opponent.
-
-//    * When the defender's `HP` is reduced to zero or below, remove the enemy from the `defender area`. The player character can now choose a new opponent.
-
-// 4. The player wins the game by defeating all enemy characters. The player loses the game the game if their character's `HP` falls to zero or below.
-
-// ##### Option 2 Game design notes
-
-// * Each character in the game has 3 attributes: `Health Points`, `Attack Power` and `Counter Attack Power`.
-
-// * Each time the player attacks, their character's Attack Power increases by its base Attack Power. 
-//   * For example, if the base Attack Power is 6, each attack will increase the 
-//Attack Power by 6 (12, 18, 24, 30 and so on).
-// * The enemy character only has `Counter Attack Power`. 
-
-//   * Unlike the player's `Attack Points`, `Counter Attack Power` never changes.
-
-// * The `Health Points`, `Attack Power` and `Counter Attack Power` of each character must differ.
-
-// * No characters in the game can heal or recover Health Points. 
-
-//   * A winning player must pick their characters wisely by first fighting an enemy with low `Counter Attack Power`. This will allow them to grind `Attack Power` and to take on enemies before they lose all of their `Health Points`. Healing options would mess with this dynamic.
-
-// * Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
